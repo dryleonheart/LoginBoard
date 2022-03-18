64ce27e14ec8de3d81aacd4a9d91b8657304a8b1
dryleonheart <fatmouse25@gmail.com>
2022-03-18 15:01:28 +0900

    init

---
+++ src/main/java/com/board/loginboard/LoginBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.loginboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginBoardApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/board/loginboard/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.board.loginboard;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LoginBoardApplication.class);
+    }
+
+}
